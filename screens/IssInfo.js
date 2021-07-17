4236ca2e750bdaa77b3152cee6af6317e91c8678
HarshitNischal <71174516+HarshitNischal@users.noreply.github.com>
2021-07-17 20:02:30 +0530

    Add files via upload

--- screens/IssInfo.js
+++ screens/IssInfo.js
@@ -23,6 +23,10 @@ export default class IssLoacationScreen extends Component{
     }
     componentDidMount(){
         this.getIssLocation()
+        try{setInterval(async()=>{
+            this.getIssLocation()
+        },5000)}
+        catch(e){console.log(e)}
     }
     render(){
         if(Object.keys(this.state.location).length===0){
@@ -37,32 +41,8 @@ export default class IssLoacationScreen extends Component{
         else{
             return(
                 <View style={styles.container}>
-                    <SafeAreaView style={styles.androidSafeAreaView}/>
-                    <ImageBackground source={require('../assets/iss_bg.jpg')} style={{
-                        flex:1,
-                        resizeMode:"cover"
-                        }}>
-               <View style={styles.TitleBar}>
-                <Text style={styles.TitleText}>
-                     Iss Location
-                 </Text> 
-                 </View>
-                    <MapView
-                    style={styles.map}
-                    region={{
-                    latitude: this.state.location.latitude,
-                    longitude: this.state.location.longitude,
-                    latitudeDelta: 100,
-                    longitudeDelta: 100,
-                    }}
-                >
-                    <Marker coordinate={{  latitude: this.state.location.latitude, longitude: this.state.location.longitude }}>
-                    <Image source={require('../assets/iss_icon.png')} style={{
-                    hieght:50,
-                    width:50
-                      }}/>
-                            </Marker>
-                </MapView>
+                   
+                    
                        <Text style={styles.routeText}>
                    Latitude:{this.state.location.latitude}
                </Text>
@@ -76,8 +56,7 @@ export default class IssLoacationScreen extends Component{
                Velocity(KM/hr):{this.state.location.velocity}
                </Text>
                     
-              
-                 </ImageBackground>
+                
                  </View>)
 
 
@@ -86,22 +65,11 @@ export default class IssLoacationScreen extends Component{
 }
 const styles=StyleSheet.create({
     container:{flex:1},
-    TitleBar:{
-        flex:0.15,
-        justifyContent:'center',
-        alignItems:'center'
-    },
-    TitleText:{
-        fontSize:40,
-    fontWeight:"bold",
-color:'black'},
-androidSafeAreaView:{marginTop:Platform.OS==='android'?StatusBar.currentHeight:0},
-
+  
 RouteText:{
     fontSize:15,
 fontWeight:"bold",
 color:'black',
 },
 
-   map:{width:'100%',hieght:'100%'}
 })
